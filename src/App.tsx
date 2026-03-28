4efac7499d96a992d5e9a85b08a32cba65b50d1b
3lawie <mly445032@gmail.com>
2026-03-29 00:09:52 +0300

    placing videos test

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,12 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col gap-[250px] relative top-[200px] ">
-        <video src=""></video>
-        <video src=""></video>
+      <div className="h-full flex flex-col gap-[250px] relative top-[200px] mb-[200px] p-10">
+        <video src="videos/زيارة.mp4" controls
+          className='rounded-2xl w-[500px] h-[600px] object-contain m-auto'></video>
+        <video src="videos/3 HOUR STUDY WITH ME  Background noise, Rain Sounds, 10-min break, No Music - Merve (1080p, h264, youtube).mp4"
+          controls
+          className='border-2 rounded-2xl'></video>
       </div>
     </>
   )
