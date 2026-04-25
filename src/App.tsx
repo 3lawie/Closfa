ff0897453f096bceab08ff227d57744772a4490c
3lawie <mly445032@gmail.com>
2026-04-25 15:55:27 +0300

    set UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import ImageRenderer from "./Media Handler/ImageRenderer";
 
 function App() {
   const {
@@ -18,7 +19,7 @@ function App() {
 
   if (isLoading) return "Loading...";
 
-  return isAuthenticated ? (
+  return <> ( {isAuthenticated} ? (
     <>
       <p>Logged in as {user!.email}</p>
 
@@ -28,7 +29,7 @@ function App() {
 
       <button onClick={logout}>Logout</button>
     </>
-  ) : (
+    ) : (
     <>
       {error && <p>Error: {error.message}</p>}
 
@@ -36,7 +37,17 @@ function App() {
 
       <button onClick={login}>Login</button>
     </>
-  )
+    )
+    )
+    <div className="h-full flex flex-col gap-[250px] relative top-[200px] mb-[200px] p-10">
+      <video src="videos/زيارة.mp4" controls
+        className='rounded-2xl w-[500px] h-[600px] object-contain m-auto'></video>
+      <video src="videos/3 HOUR STUDY WITH ME  Background noise, Rain Sounds, 10-min break, No Music - Merve (1080p, h264, youtube).mp4"
+        controls
+        className='border-2 rounded-2xl'></video>
+      <ImageRenderer />
+    </div>
+  </>
 
 
     ;
