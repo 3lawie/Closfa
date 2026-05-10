6278d78d9d53b75ae51bcd362e2cebadf2649a5a
3lawie <mly445032@gmail.com>
2026-05-10 10:54:26 +0300

    update the compiler and the connection helper

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,5 @@
-import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { schema } from "./schema";
 import { relations } from "./realtions";
 
-const client = postgres(process.env.DATABASE_URL!);
-export const db = drizzle(client, { schema, relations });
+export const db = drizzle(process.env.DATABASE_URL!, { schema, relations });
